fafe6a6fc1cb51a24f1438b4ceca06f2740c72e3
Hutaba-Makura <152959976+Hutaba-Makura@users.noreply.github.com>
2025-07-04 23:43:14 +0900

    Fix:ふぁぼやRTの検知

--- src/contentScript/apiInterceptor.ts
+++ src/contentScript/apiInterceptor.ts
@@ -28,7 +28,7 @@ const requestPathWeakMap = new WeakMap<XMLHttpRequest, TxTarget>();
 
 const Pattern = Object.freeze({
   tweetRelated:
-    /^(?:\/i\/api)?\/graphql\/(?<queryId>.+)?\/(?<queryName>TweetDetail|TweetResultByRestId|UserTweets|UserMedia|HomeTimeline|HomeLatestTimeline|UserTweetsAndReplies|UserHighlightsTweets|UserArticlesTweets|Bookmarks|Likes|CommunitiesExploreTimeline|ListLatestTweetsTimeline|SearchTimeline|UserByScreenName|UserByRestId)$/,
+    /^(?:\/i\/api)?\/graphql\/(?<queryId>.+)?\/(?<queryName>TweetDetail|TweetResultByRestId|UserTweets|UserMedia|HomeTimeline|HomeLatestTimeline|UserTweetsAndReplies|UserHighlightsTweets|UserArticlesTweets|Bookmarks|Likes|CommunitiesExploreTimeline|ListLatestTweetsTimeline|SearchTimeline|UserByScreenName|UserByRestId|FavoriteTweet|CreateRetweet)$/,
 });
 
 
